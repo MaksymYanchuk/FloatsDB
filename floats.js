c166a8827e2e4c6b6398d343ddddbb221e2d4c40
Maksym Yanchuk <113847081+MaksymYanchuk@users.noreply.github.com>
2026-01-24 17:04:00 +0000

    added printstream set

--- floats.js
+++ floats.js
@@ -1,4 +1,41 @@
-    const floatConfig = {
-        "AWP Asiimov": { "FT": 0.24 },
-        default: { "FN": 0.02, "MW": 0.09, "FT": 0.17 }
-    };
+const floatConfig = {
+
+    // Knives
+    "★ Bowie Knife Tiger Tooth": { "FN": 0.01 },
+
+    //Pistols
+    "USP-S Printstream": {
+        "FN": 0.025,
+        "MW": 0.09
+    },
+
+    "Desert Eagle Printstream": {
+        "FN": 0.025,
+        "MW": 0.075
+    },
+
+    // Assault Rifles
+    "AK-47 Inheritance": {
+        "FN": 0.02,
+        "MW": 0.08
+    },
+
+    "M4A1-S Printstream": {
+        "FN": 0.02,
+        "MW": 0.08
+    },
+
+    // Sniper Rifles
+    "AWP Asiimov": { "FT": 0.24 },
+
+    "AWP Printstream": {
+        "FN": 0.01,
+        "MW": 0.08
+    },
+
+
+    default: { "FN": 0.02, "MW": 0.09, "FT": 0.17 }
+};
+
+
+
